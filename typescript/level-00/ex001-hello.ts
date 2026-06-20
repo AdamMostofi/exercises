f29ff17ca60d069e5a1ceb047b833ec99caa06e5
Adam Mostofi <adammostofi@gmail.com>
2026-06-20 14:07:41 +0300

    feat(ts): Level-0 ex001 - Hello, TypeScript!

---
+++ typescript/level-00/ex001-hello.ts
@@ -0,0 +1,11 @@
+/**
+ * ex001 - Hello, TypeScript!
+ *
+ * Task: Declare a variable `greeting` with type `string`
+ * and value "Hello, TypeScript!". Then log it.
+ *
+ * EXPECTED OUTPUT:
+ * Hello, TypeScript!
+ */
+
+// Your code below:
