c263ac3c6172c42e56fc5dffa0d9833b2dbbba79
Adam Mostofi <adammostofi@gmail.com>
2026-06-20 17:28:20 +0300

    feat(py): Level-0 ex010 - variable reassignment

---
+++ python/level-00/ex010-reassign.py
@@ -0,0 +1,12 @@
+"""
+ex010 - Reassign
+
+Task: Create a variable `score` with value 10.
+      Then reassign it to 20.
+      Print score after the reassignment.
+
+EXPECTED OUTPUT:
+20
+"""
+
+# Your code below:
