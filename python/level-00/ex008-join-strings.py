2e939f7c02fbdac1885d4cf5119a8114d93a07c0
Adam Mostofi <adammostofi@gmail.com>
2026-06-20 17:28:16 +0300

    feat(py): Level-0 ex008 - join two strings

---
+++ python/level-00/ex008-join-strings.py
@@ -0,0 +1,13 @@
+"""
+ex008 - Join Strings
+
+Task: Two variables hold strings. Concatenate them with a space
+      in between and print the result.
+
+EXPECTED OUTPUT:
+Hello World
+"""
+
+# Your code below:
+a = "Hello"
+b = "World"
