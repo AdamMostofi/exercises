b2d15e0b96f771cad688ef090595b2a9c2a13fe4
Adam Mostofi <adammostofi@gmail.com>
2026-06-20 14:08:24 +0300

    feat(js): Level-0 ex002 - CLI Arguments

---
+++ nodejs/level-00/ex002-cli-args.js
@@ -0,0 +1,22 @@
+/**
+ * ex002 - CLI Arguments
+ *
+ * Task: Print the CLI arguments passed to this script.
+ *       Use process.argv to access them.
+ *
+ * Run with:
+ *   node nodejs/level-00/ex002-cli-args.js hello 42
+ *
+ * The first two elements are always the node path and script path.
+ * You just need to print process.argv as-is.
+ *
+ * EXPECTED OUTPUT (when run with: node ex002-cli-args.js hello 42):
+ * [
+ *   '/usr/bin/node',
+ *   '/path/to/ex002-cli-args.js',
+ *   'hello',
+ *   '42'
+ * ]
+ */
+
+// Your code below:
