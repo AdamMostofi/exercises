b82646a2a46ab4c3a4da317c5ddc6002fa55c787
Adam Mostofi <adammostofi@gmail.com>
2026-06-20 17:28:15 +0300

    feat(py): Level-0 ex007 - concat int to string

---
+++ python/level-00/ex007-concat-int.py
@@ -0,0 +1,14 @@
+"""
+ex007 - Concatenate Int to String
+
+Task: You have a number 2026. Convert it to a string and
+      concatenate it with "Year " to print: Year 2026
+
+HINT: Use str() to convert the number.
+
+EXPECTED OUTPUT:
+Year 2026
+"""
+
+# Your code below:
+year = 2026
