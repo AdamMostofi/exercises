9a3fa9d2688f6b2b34a31b7c0f6140babc2ea1d0
Adam Mostofi <adammostofi@gmail.com>
2026-06-20 14:21:22 +0300

    solve(sql): ex003 - INSERT Diana

--- sql/level-00/ex003-insert-row.py
+++ sql/level-00/ex003-insert-row.py
@@ -26,10 +26,10 @@ cur.execute("INSERT INTO users VALUES (3, 'Charlie', 22)")
 conn.commit()
 
 # Write your INSERT query below (as a string):
-insert_query = ""
+insert_query = "INSERT INTO users (id,name,age) VALUES (4, 'Diana', 28)"
 
 # Write your SELECT query below (to verify):
-select_query = ""
+select_query = "SELECT * FROM users"
 
 # --- don't modify below ---
 cur.execute(insert_query)
