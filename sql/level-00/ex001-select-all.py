7fdc501bdbd5368b473abd3d7bc12f749f83e6d5
Adam Mostofi <adammostofi@gmail.com>
2026-06-20 14:21:18 +0300

    solve(sql): ex001 - SELECT * FROM users

--- sql/level-00/ex001-select-all.py
+++ sql/level-00/ex001-select-all.py
@@ -23,7 +23,7 @@ cur.execute("INSERT INTO users VALUES (3, 'Charlie', 22)")
 conn.commit()
 
 # Write your SQL query below (as a string):
-query = ""
+query = "SELECT * FROM users"
 
 # --- don't modify below ---
 cur.execute(query)
