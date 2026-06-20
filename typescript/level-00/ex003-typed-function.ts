7f5689f7eb8218832e0d83d15ee1912210e7bcc8
Adam Mostofi <adammostofi@gmail.com>
2026-06-20 14:07:47 +0300

    feat(ts): Level-0 ex003 - Typed Function

---
+++ typescript/level-00/ex003-typed-function.ts
@@ -0,0 +1,11 @@
+/**
+ * ex003 - Typed Function
+ *
+ * Task: Write a function `add` that takes two `number` params
+ * and returns a `number`. Then log `add(10, 20)`.
+ *
+ * EXPECTED OUTPUT:
+ * 30
+ */
+
+// Your code below:
