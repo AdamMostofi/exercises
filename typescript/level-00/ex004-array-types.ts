1e992d110ca6b679fb301b8d6186586cc9832f37
Adam Mostofi <adammostofi@gmail.com>
2026-06-20 14:07:47 +0300

    feat(ts): Level-0 ex004 - Array Types

---
+++ typescript/level-00/ex004-array-types.ts
@@ -0,0 +1,14 @@
+/**
+ * ex004 - Array Types
+ *
+ * Task:
+ *   1. Declare an array of numbers: `[1, 2, 3, 4, 5]` with a type annotation
+ *   2. Log the array
+ *   3. Log the length of the array
+ *
+ * EXPECTED OUTPUT:
+ * [ 1, 2, 3, 4, 5 ]
+ * 5
+ */
+
+// Your code below:
