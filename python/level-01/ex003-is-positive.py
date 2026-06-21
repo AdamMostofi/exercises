4a92a2375d8588f8d8c3544d0025875a5ef84d66
Adam Mostofi <adammostofi@gmail.com>
2026-06-21 12:41:42 +0300

    feat(py): Level-1 ex003 - is_positive function

---
+++ python/level-01/ex003-is-positive.py
@@ -0,0 +1,12 @@
+"""
+ex003 - Is Positive?
+
+Task: Write a function `is_positive` that takes a number
+      and returns True if it's greater than 0, False otherwise.
+      Then call it with value -3 and print the result.
+
+EXPECTED OUTPUT:
+False
+"""
+
+# Your code below:
