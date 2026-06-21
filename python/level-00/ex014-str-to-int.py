35a8a33f0ff7e5d2517470528301fbf07c13c194
Adam Mostofi <adammostofi@gmail.com>
2026-06-21 12:41:16 +0300

    feat(py): Level-0 ex014 - convert string to int

---
+++ python/level-00/ex014-str-to-int.py
@@ -0,0 +1,13 @@
+"""
+ex014 - String to Int
+
+Task: You have a string "50" and an integer 25.
+      Convert the string to int, add them, and print the result.
+
+EXPECTED OUTPUT:
+75
+"""
+
+# Your code below:
+num_str = "50"
+num_int = 25
