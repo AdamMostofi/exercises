1cf43d4a77b25985524d5ea6d6ef1defdeaed1eb
Adam Mostofi <adammostofi@gmail.com>
2026-06-21 12:41:38 +0300

    feat(py): Level-1 ex002 - print 1 to 5

---
+++ python/level-01/ex002-print-1-to-n.py
@@ -0,0 +1,17 @@
+"""
+ex002 - Print 1 to N
+
+Task: Use a for loop with range() to print numbers
+      1 through 5, each on its own line.
+
+HINT: range(1, 6) gives you 1, 2, 3, 4, 5
+
+EXPECTED OUTPUT:
+1
+2
+3
+4
+5
+"""
+
+# Your code below:
