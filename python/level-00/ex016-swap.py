694fc7f2e65090fee75413b8f8c838c3fc2b6bc2
Adam Mostofi <adammostofi@gmail.com>
2026-06-21 12:41:22 +0300

    feat(py): Level-0 ex016 - swap variables

---
+++ python/level-00/ex016-swap.py
@@ -0,0 +1,14 @@
+"""
+ex016 - Swap Variables
+
+Task: Swap the values of `a` and `b` so that a becomes 5 and b becomes 3.
+      Print a and b after swapping, each on its own line.
+
+EXPECTED OUTPUT:
+5
+3
+"""
+
+# Your code below:
+a = 3
+b = 5
